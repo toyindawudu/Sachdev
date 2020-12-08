67f27f8a20c86b7f55961fb2255a4655e7957e74
Toyin Dawudu <toyindawudu@gmail.com>
2020-12-08 10:41:54 +0000

    Made styling and functional changes to the mobile menu hamburger

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElements = document.querySelectorAll('.has-fade')
@@ -6,6 +7,7 @@ const fadeElements = document.querySelectorAll('.has-fade')
 btnHamburger.addEventListener('click', function() {
   
   if(header.classList.contains('open')){ // closes hamburger menu
+    body.classList.remove('noscroll');
     header.classList.remove('open'); //remove the class list open
     fadeElements.forEach(function(element){
       element.classList.add('fade-out');
@@ -13,12 +15,11 @@ btnHamburger.addEventListener('click', function() {
     });
     
   } else {                          // open hamburger menu
+    body.classList.add('noscroll');
     header.classList.add('open'); //else add the class list open 
     fadeElements.forEach(function(element){
       element.classList.remove('fade-out');
       element.classList.add('fade-in');
     });
-    
   }
-
 });
